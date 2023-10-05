1e311b72b9adb7352b80aa65af7666f0473bb8ea
Arthurdevweb <arthuraugusto176@gmail.com>
2023-10-05 15:04:03 -0300

    sintaxe do js

---
+++ README.md
@@ -0,0 +1,14 @@
+# Atividades com a alinguagem JavaScript 
+
+## Sobre a linguagem
+
+- JavaScript NÃO É JAVA!
+- JavaScript pode ser abreviado como JS
+- É uma linguagem voltada principalmente para o Front-end
+- Mas também pode ser usado no Back-end através do Node.js
+- JS pode ser usado para criar recursos interativos nos sites, como por exemplo:
+    - Galeria de fotos
+    - Carousel/Slider de conteúdo 
+    - Funcionalidades de navegação 
+    - Validações mais avançadas de formulário
+- JS pode ser usado até mesmo para criação de aplicativos e games. 
---
+++ js/01-sintaxe.js
@@ -0,0 +1,31 @@
+console.log("Programador Web");
+console.log('Senac Penha');
+
+/* Comentário - ALT + SHIFT + A  */
+
+// Comentário - CTRL ; 
+
+/* Manipulando dados */
+
+// Variáveis
+
+let curso = "HTML5 e CSS3";
+let ano = 2023; 
+
+//Constantes
+
+const MediaMinima = 7; 
+
+//Saída de dados a partir da memória
+
+console.log(curso);
+console.log(ano);
+console.log(MediaMinima);
+
+/* Estou fazendo o curso de HTML5 e CSS3 neste ano de 2023  */
+
+//Template String/Literal: saída usando a crase e ${} 
+console.log(`Estou fazendo o curso de ${curso} neste ano de ${ano}`);
+
+// Concatenação 
+console.log("Estou fazendo o curso de "+curso+" neste ano de "+ano);
