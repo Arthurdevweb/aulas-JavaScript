8f616c174b81c7571ade47f3cd90fb08a4867f80
Arthurdevweb <arthuraugusto176@gmail.com>
2023-10-17 14:53:59 -0300

    explicação funções no JS

---
+++ js/06-funcoes.js
@@ -0,0 +1,16 @@
+"use strict"
+
+/*  Sobre funções 
+
+Funções são blocos de códigos que podem ser executados e reaproveitados em praticamente qualquer lugar de uma aplicação/site.
+
+Usar funções também é benéfico do ponto de vista de organização de código.
+
+O JS já possui centenas de funções prontas.
+Exemplos: log(), alert(), prompt(), toFixed() etc*/
+
+
+
+
+
+
