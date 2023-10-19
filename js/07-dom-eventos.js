341d1491f19e69cfb122a2eed2cb4c68707dc6ff
Arthurdevweb <arthuraugusto176@gmail.com>
2023-10-19 14:30:26 -0300

    manipulação com DOM

---
+++ js/07-dom-eventos.js
@@ -0,0 +1,79 @@
+"use strict";
+
+/* 
+Funções de seleção no DOM
+
+-getElementById() // Seleciona UM elemento através de um id.
+
+
+-querySelector() // Seleciona UM elemento através de um seletor.
+
+-querySelectorAll() // Seleciona VÁRIOS elementos através de um/vários seletores.
+
+*/
+
+
+// Exemplos
+
+const subtitulo = document.getElementById("subtitulo-dom")
+console.log(subtitulo);
+
+const desenho = document.querySelector("figure")
+console.log(desenho);
+
+const descricao = document.querySelector("#descricao")
+console.log(descricao);
+
+const variosElementos = document.querySelectorAll("h3, a")
+console.log(variosElementos);
+console.log(variosElementos[1]);
+
+
+
+console.log("----------------------------------------------");
+
+// Modificando o DOM
+const titulo = document.querySelector("h1")
+titulo.textContent = "Olá JavaScript!"; // Content via JS
+titulo.style.textAlign = "center"; // CSS inline via JS
+titulo.classList.add("destaque"); // Classe CSS via JS
+
+/* Selecionando os links contidos na lista */
+const linksReferencia = document.querySelectorAll("ul li a");
+console.log(linksReferencia);
+
+let quantidadeDeLinks = linksReferencia.length;
+
+// for(let i = 0; i < quantidadeDeLinks; i++){
+//     linksReferencia[i].setAttribute("target", "_blank");
+// }
+
+for(let link of linksReferencia){
+    link.setAttribute("target", "_blank");
+}
+
+
+
+console.log("----------------------------------------------");
+
+// Manipulando Eventos
+const pagina = document.querySelector("body");
+const exemplo01 = document.querySelector("#exemplo01");
+const mensagem = document.querySelector("#mensagem");
+
+const reprodutorAudio = document.querySelector("#reprodutor-audio");
+const imagemMeme = document.querySelector("#imagem-meme")
+
+//  Ouvinte de evento
+exemplo01.addEventListener("click", function(){
+    pagina.style.fontFamily = "Verdana";
+    mensagem.textContent = "Fonte alterada!"
+    mensagem.style.backgroundColor = "yellow"
+
+    //Executamos o adios com play 
+    reprodutorAudio.play();
+
+    //Removemos o atributo hidden (portanto, a imagem aparecerá)
+    imagemMeme.removeAttribute("hidden")
+
+});
